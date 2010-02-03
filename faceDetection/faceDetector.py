37746352e2eb3ce592fc1c4d2903eae033c052cc
guillem mateu mascaro <liceu@mestral.example.com>
2010-02-03 10:19:53 +0100

    imatges redimensionades per evitar deformacions al gestib

--- faceDetection/faceDetector.py
+++ faceDetection/faceDetector.py
@@ -8,14 +8,17 @@ from opencv.highgui import *
 DIR_FOTOS="/home/liceu/Imatges/Fotografies/2010/01/28/" #directori on hi ha les fotos, el darrer "/" és important
 DIR_DESTI="/home/liceu/Imatges/curs2009/" #directori on es guarden els rostres, el darrer "/" és important
 PATRO_XML="haarcascade_frontalface_alt.xml" #patrons a buscar
-MARGE_LATERAL=100 #les cares s'han d'ampliar tan cap als laterals com en la zona vertical
-MARGE_VERTICAL=150
+MARGE_LATERAL=70 #les cares s'han d'ampliar tan cap als laterals com en la zona vertical
+MARGE_VERTICAL=190
+xRATIO=480
+yRATIO=640
 hResized=150 #alçada de les imatges redimensionades
+RESIZE=True #en aquest cas redimensinarem les cares
+RENAME=True #en aquest cas reanomenam les captures segons l'ordre en que es van prendre
 
-def detectObjects(image,k):
+def detectObjects(image,k,file):
      grayscale = cvCreateImage(cvSize(image.width, image.height), 8, 1)
      cvCvtColor(image, grayscale, CV_BGR2GRAY)
-     
      storage = cvCreateMemStorage(0)
      cvClearMemStorage(storage)
      cvEqualizeHist(grayscale, grayscale)
@@ -25,29 +28,35 @@ def detectObjects(image,k):
      if faces:
          for f in faces:
              img = adaptors.Ipl2PIL(image)
-             
-
              box = (f.x-MARGE_LATERAL, f.y-MARGE_VERTICAL, f.x+f.width+MARGE_LATERAL, f.y+f.height+MARGE_VERTICAL)
+             #redimensionam la capsa per evitar deformacions a l'hora de pujar al gestib
+             faceY=box[3]-box[1]
+             faceX=(faceY*xRATIO)/yRATIO
+             box=(f.x-MARGE_LATERAL, f.y-MARGE_VERTICAL, f.x+faceX,f.y+f.height+MARGE_VERTICAL)
              region = img.crop(box)
              factor=float(hResized)/region.size[1]
              wResized=region.size[0]*factor
              sizeRedim=(int(wResized),int(hResized))
-             region=region.resize(sizeRedim)   
-             print "region.size",region.size,"sizeRedim",sizeRedim
+             if RESIZE:
+                region=region.resize(sizeRedim)   
+                print "region.size",region.size,"sizeRedim",sizeRedim
              i=i+1
-             region.save(DIR_DESTI+str(k)+'_'+str(i)+'.jpg')
+             nom=DIR_DESTI+file
+             if RENAME:
+                 nom=DIR_DESTI+str(k)+'_'+str(i)+'.jpg'    
+                 print "rename nom",nom
+             region.save(nom)
  
 def main():
     k=0
     for root,dirs,files in os.walk(DIR_FOTOS):
         files.sort()
         for file in [f for f in files if f.lower().endswith("jpg")]:
-            file=DIR_FOTOS+file
-            image = cvLoadImage(file, 1)
+            picture=DIR_FOTOS+file
+            image = cvLoadImage(picture, 1)
             k=k+1
-            print k,file
-            detectObjects(image,k)
+            print k,picture,"file",file
+            detectObjects(image,k,file)
  
 if __name__ == "__main__":
   main()
-
